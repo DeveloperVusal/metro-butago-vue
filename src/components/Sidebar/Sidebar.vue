307f9f531d44ffb2ab8fe46f4d1bf5cf052d1666
DeveloperVusal <developer.vusal96@gmail.com>
2022-10-05 01:06:24 +0400

    refactor(sidebar): change scroll to custom scrollbar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,9 +1,11 @@
 <script>
+import vueCustomScrollbar from 'vue-custom-scrollbar/src/vue-scrollbar.vue'
+
 import './styles/Sidebar.scss'
 
 import IconSwapSVG from '@/components/Icons/IconSwap.vue'
 import IconCloseSVG from '@/components/Icons/IconClose.vue'
-import IconWalkerSVG from '../Icons/IconWalker.vue'
+import IconWalkerSVG from '@/components/Icons/IconWalker.vue'
 
 import { useRoutesStore } from '@/stores/routes'
 import { useStationsStore } from '@/stores/stations'
@@ -12,7 +14,8 @@ export default {
     components: {
         IconSwapSVG,
         IconCloseSVG,
-        IconWalkerSVG
+        IconWalkerSVG,
+        vueCustomScrollbar
     },
     setup() {
         const store = useRoutesStore()
@@ -99,6 +102,12 @@ export default {
             route_to: {
                 name: '',
                 color: ''
+            },
+            settings: {
+                suppressScrollY: false,
+                suppressScrollX: false,
+                wheelPropagation: false,
+                wheelSpeed: 0.3,
             }
         }
     }
@@ -106,111 +115,115 @@ export default {
 </script>
 
 <template>
-    <div class="sidebar sidebar--scroll">
-        <div class="sidebar__top">
-            <div class="sidebar__center sidebar__center--fixed">
-                <div class="logo">
-                    <img src="@/assets/images/logo-text.svg" alt="">
-                </div>
-            </div>
-        </div>
-
-        <div class="sidebar__points">
-            <div class="points sidebar__center">
-                <div class="points__stations">
-                    <div class="points__from_to">
-                        <div 
-                            class="points__dot_line" 
-                            :style="(route_from.color) ? 'background-color: ' + route_from.color : ''"
-                        ></div>
-                        <div class="points__input">
-                            <input type="text" placeholder="Hardan" :value="route_from.name">
-                            <IconCloseSVG :from="null" v-show="store.getRoute.from" :to="store.getRoute.to" />
-                        </div>
-                    </div>
-                    <div class="points__from_to">
-                        <div 
-                            class="points__dot_line" 
-                            :style="(route_to.color) ? 'background-color: ' + route_to.color : ''"
-                        ></div>
-                        <div class="points__input">
-                            <input type="text" placeholder="Haraya" :value="route_to.name">
-                            <IconCloseSVG :to="null" v-show="store.getRoute.to" :from="store.getRoute.from" />
+    <div class="sidebar">
+        <vue-custom-scrollbar class="scroll-area"  :settings="settings">
+            <div class="sidebar__box">
+                <div class="sidebar__top">
+                    <div class="sidebar__center sidebar__center--fixed">
+                        <div class="logo">
+                            <img src="@/assets/images/logo-text.svg" alt="">
                         </div>
                     </div>
                 </div>
-                <div 
-                    :class="(route_from.name || route_to.name) ? 'points__reverse points__reverse--active' : 'points__reverse'"
-                    @click="store.setReverseRoute()"
-                >
-                    <IconSwapSVG />
-                </div>
-            </div>
-        </div>
 
-        <div class="sidebar__route-reset sidebar__center" v-if="routes.time > 0">
-            <button
-                class="route-reset__button" 
-                type="button"
-                @click="fnResetRoute()"
-            >Marşrutu sıfırlayın</button>
-        </div>
-
-        <div class="sidebar__routes" v-if="routes.time > 0">
-            <div class="routes sidebar__center">
-                <ul class="routes__list">
-                    <li class="routes__route">
-                        <div class="routes__times">
-                            <span class="routes-times__time">{{ routes.time }} dəqiqə</span>
-                            <span v-if="(routes.transfers)">
-                                —
-                                <span class="routes-times__transfer">
-                                    {{ routes.transfers }} keçid
-                                </span>
-                            </span>
-                        </div>
-                        <div class="routes__path">
-                            <div class="route-path">
-                                <div
-                                    v-for="path,key in routes.paths"
-                                    :key="key"
-                                >
-                                    <ul 
-                                        :class="`
-                                            route-path__move route-path__move--color-line_${path.line_id}
-                                        `"
-                                    >
-                                        <div class="route-path__time">
-                                            <div>{{ path.stationCount }} stan</div>
-                                            <div>{{ path.times.move }} dəq</div>
-                                        </div>
-
-                                        <li 
-                                            v-for="station,indx in path.stations"
-                                            :class="`
-                                                station ${(indx == 0) ? 'station--first-last' : ''} ${((indx + 1) == path.stations.length && path.stations.length > 1) ? 'station--first-last' : ''}
-                                            `"
-                                            :key="station.id"
-                                        >
-                                            <span class="station__line"></span>
-                                            <div class="station__text">{{ station.name }}</div>
-                                        </li>
-                                    </ul>
-                                    <div
-                                        v-if="(key != Object.keys(routes.paths).slice(-1).pop())"
-                                        class="routes-transfer"
-                                    >
-                                        <div class="routes-transfer__icon">
-                                            <IconWalkerSVG />
-                                        </div>
-                                        <div class="routes-transfer__text">Keçid — {{ path.times.transfer }} dəqiqə</div>
-                                    </div>
+                <div class="sidebar__points">
+                    <div class="points sidebar__center">
+                        <div class="points__stations">
+                            <div class="points__from_to">
+                                <div 
+                                    class="points__dot_line" 
+                                    :style="(route_from.color) ? 'background-color: ' + route_from.color : ''"
+                                ></div>
+                                <div class="points__input">
+                                    <input type="text" placeholder="Hardan" :value="route_from.name">
+                                    <IconCloseSVG :from="null" v-show="store.getRoute.from" :to="store.getRoute.to" />
+                                </div>
+                            </div>
+                            <div class="points__from_to">
+                                <div 
+                                    class="points__dot_line" 
+                                    :style="(route_to.color) ? 'background-color: ' + route_to.color : ''"
+                                ></div>
+                                <div class="points__input">
+                                    <input type="text" placeholder="Haraya" :value="route_to.name">
+                                    <IconCloseSVG :to="null" v-show="store.getRoute.to" :from="store.getRoute.from" />
                                 </div>
                             </div>
                         </div>
-                    </li>
-                </ul>
+                        <div 
+                            :class="(route_from.name || route_to.name) ? 'points__reverse points__reverse--active' : 'points__reverse'"
+                            @click="store.setReverseRoute()"
+                        >
+                            <IconSwapSVG />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sidebar__route-reset sidebar__center" v-if="routes.time > 0">
+                    <button
+                        class="route-reset__button" 
+                        type="button"
+                        @click="fnResetRoute()"
+                    >Marşrutu sıfırlayın</button>
+                </div>
+
+                <div class="sidebar__routes" v-if="routes.time > 0">
+                    <div class="routes sidebar__center">
+                        <ul class="routes__list">
+                            <li class="routes__route">
+                                <div class="routes__times">
+                                    <span class="routes-times__time">{{ routes.time }} dəqiqə</span>
+                                    <span v-if="(routes.transfers)">
+                                        —
+                                        <span class="routes-times__transfer">
+                                            {{ routes.transfers }} keçid
+                                        </span>
+                                    </span>
+                                </div>
+                                <div class="routes__path">
+                                    <div class="route-path">
+                                        <div
+                                            v-for="path,key in routes.paths"
+                                            :key="key"
+                                        >
+                                            <ul 
+                                                :class="`
+                                                    route-path__move route-path__move--color-line_${path.line_id}
+                                                `"
+                                            >
+                                                <div class="route-path__time">
+                                                    <div>{{ path.stationCount }} stan</div>
+                                                    <div>{{ path.times.move }} dəq</div>
+                                                </div>
+
+                                                <li 
+                                                    v-for="station,indx in path.stations"
+                                                    :class="`
+                                                        station ${(indx == 0) ? 'station--first-last' : ''} ${((indx + 1) == path.stations.length && path.stations.length > 1) ? 'station--first-last' : ''}
+                                                    `"
+                                                    :key="station.id"
+                                                >
+                                                    <span class="station__line"></span>
+                                                    <div class="station__text">{{ station.name }}</div>
+                                                </li>
+                                            </ul>
+                                            <div
+                                                v-if="(key != Object.keys(routes.paths).slice(-1).pop())"
+                                                class="routes-transfer"
+                                            >
+                                                <div class="routes-transfer__icon">
+                                                    <IconWalkerSVG />
+                                                </div>
+                                                <div class="routes-transfer__text">Keçid — {{ path.times.transfer }} dəqiqə</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
+        </vue-custom-scrollbar>
     </div>
 </template>
