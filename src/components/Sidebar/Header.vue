5974ea7b3fe5ea0b3bb5332f867970c8d6cb3010
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-03 15:52:30 +0300

    refactor: get main domain from env file

--- src/components/Sidebar/Header.vue
+++ src/components/Sidebar/Header.vue
@@ -1,11 +1,19 @@
 <script>
 import './styles/Header.scss'
+
+export default {
+    data() {
+        return {
+            main_url: import.meta.env.VITE_MAIN_DOMAIN
+        }
+    }
+}
 </script>
 
 <template>
     <div class="header">
         <div class="sidebar__center sidebar__center--fixed">
-            <a href="https://butago.com/" target="_blank">
+            <a :href="main_url">
                 <div class="logo">
                     <img src="@/assets/images/logo-text.svg" alt="">
                 </div>
